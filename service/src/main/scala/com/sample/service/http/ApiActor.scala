989e539f81a4a6d362b0159d8a2ec063a8199159
Dmitry Ivanov <id.ajantis@gmail.com>
2014-06-05 16:22:17 +0200

    Scaffolded Rest API actor implementation

--- service/src/main/scala/com/sample/service/http/ApiActor.scala
+++ service/src/main/scala/com/sample/service/http/ApiActor.scala
@@ -2,8 +2,34 @@ package com.sample.service
 package http
 
 import akka.actor.ActorLogging
-import spray.routing.{ HttpService, HttpServiceActor }
+import spray.routing.{ Route, HttpService, HttpServiceActor }
+import spray.http.StatusCodes
 
 class ApiActor(settings: Settings) extends HttpServiceActor with HttpService with ActorLogging {
-  def receive = ???
+  import ApiActor._
+
+  private[http] val timeout = settings.Timeout
+
+  def receive = runRoute(route)
+
+  private[service] def route: Route = {
+    path(Ping) {
+      get {
+        complete(StatusCodes.OK -> "pong")
+      }
+    } ~
+      pathPrefix(Api) {
+        path(Service) {
+          post {
+            complete(StatusCodes.OK -> "TODO :)")
+          }
+        }
+      }
+  }
+}
+
+object ApiActor {
+  private[service] val Ping = "ping"
+  private[service] val Api = "api"
+  private[service] val Service = "service"
 }
