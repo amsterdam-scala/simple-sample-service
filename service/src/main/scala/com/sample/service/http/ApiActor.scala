d5914287536bdb967e4522b0b872505867689ccb
Dmitry Ivanov <id.ajantis@gmail.com>
2014-06-05 16:27:42 +0200

    API & job processing: connecting everything together

--- service/src/main/scala/com/sample/service/http/ApiActor.scala
+++ service/src/main/scala/com/sample/service/http/ApiActor.scala
@@ -1,12 +1,18 @@
 package com.sample.service
 package http
 
-import akka.actor.ActorLogging
-import spray.routing.{ Route, HttpService, HttpServiceActor }
+import akka.actor.{ ActorRef, Props, ActorLogging }
+
+import spray.routing.{ RequestContext, Route, HttpService, HttpServiceActor }
 import spray.http.StatusCodes
+import spray.httpx.SprayJsonSupport._
+
+import domain.SimpleJob
+import processor.ProcessingActor
 
 class ApiActor(settings: Settings) extends HttpServiceActor with HttpService with ActorLogging {
   import ApiActor._
+  import ProcessingActor._
 
   private[http] val timeout = settings.Timeout
 
@@ -21,10 +27,25 @@ class ApiActor(settings: Settings) extends HttpServiceActor with HttpService wit
       pathPrefix(Api) {
         path(Service) {
           post {
-            complete(StatusCodes.OK -> "TODO :)")
+            entity(as[SimpleJob]) { job =>
+              requestContext =>
+                val processor = createProcessor(requestContext)
+                processor ! job
+            }
           }
         }
       }
+  }
+
+  private[service] def createProcessor(ctx: RequestContext): ActorRef =
+    context.actorOf(Props(new ProcessingActor(timeout, resultCallback = completeJobRequest(ctx))))
+
+  private[service] def completeJobRequest(ctx: RequestContext)(result: JobResult): Unit = {
+    result match {
+      case Right(job)  => ctx.complete(StatusCodes.Accepted -> job)
+
+      case Left(error) => ctx.complete(StatusCodes.ServiceUnavailable -> error)
+    }
   }
 }
 
