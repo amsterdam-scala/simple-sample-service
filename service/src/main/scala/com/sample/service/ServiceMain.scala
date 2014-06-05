6f6313852d22a0809c022d8f827eeff5fe628e29
Dmitry Ivanov <id.ajantis@gmail.com>
2014-06-05 15:00:43 +0200

    Cleared the implementation

--- service/src/main/scala/com/sample/service/ServiceMain.scala
+++ service/src/main/scala/com/sample/service/ServiceMain.scala
@@ -2,39 +2,17 @@ package com.sample.service
 
 import java.util.concurrent.TimeUnit
 
-import scala.util.control.Exception.catching
 import scala.concurrent.duration._
-
-import akka.actor.{ Props, ActorSystem }
-import akka.io.IO
-
-import spray.can.Http
-
-import com.typesafe.config.{ ConfigException, Config, ConfigFactory }
-
-import http.ApiActor
+import akka.actor.ActorSystem
+import com.typesafe.config.Config
 
 object ServiceMain extends App {
   implicit val system = ActorSystem("simple-service")
 
-  catching(classOf[ConfigException]).either(Settings(ConfigFactory.load)) match {
-
-    case Right(settings) =>
-      startup(system, settings)
-
-    case Left(error) =>
-      println(s"Invalid configuration. Details: ${error.getMessage}")
-      system.shutdown()
-      system.awaitTermination(2 seconds)
-      System.exit(1)
-  }
+  // TODO setup & startup
 
   private[this] def startup(implicit system: ActorSystem, settings: Settings): Unit = {
-    val apiActor = system.actorOf(Props(new ApiActor(settings)))
-
-    IO(Http) ! Http.Bind(apiActor, interface = settings.Host, port = settings.Port)
-
-    println(s"Simple service is started. Listening on ${settings.Host}:${settings.Port}")
+    // TODO
   }
 }
 
--- service/src/main/scala/com/sample/service/processor/ProcessingActor.scala
+++ service/src/main/scala/com/sample/service/processor/ProcessingActor.scala
@@ -1,47 +1,14 @@
+
 package com.sample.service
 package processor
 
-import scala.concurrent.duration._
+import akka.actor.{ ActorLogging, Actor }
 
-import akka.actor._
-
-import domain.SimpleJob
-import ProcessingActor._
-
-class ProcessingActor(timeout: FiniteDuration, resultCallback: JobResult => Unit) extends Actor with ActorLogging {
-
-  lazy val worker = createWorker
-
-  context.setReceiveTimeout(timeout)
+class ProcessingActor extends Actor with ActorLogging {
 
   override def preStart() {
     log.debug("Starting new processor...")
   }
 
-  def receive = {
-    case job: SimpleJob    => worker ! job
-
-    case result: JobResult => finish(result)
-
-    case ReceiveTimeout =>
-      log.error("Got the timeout in processor. Aborting...")
-      finishWithError(ProcessingError("Processing failed with timeout."))
-  }
-
-  private[processor] def finish(result: JobResult): Unit = {
-    log.info("Job is done.")
-    resultCallback(result)
-    self ! PoisonPill
-  }
-
-  private[processor] def finishWithError(error: ProcessingError): Unit = {
-    resultCallback(Left(error))
-    self ! PoisonPill
-  }
-
-  private[processor] def createWorker: ActorRef = context.actorOf(Props(new Worker))
-}
-
-object ProcessingActor {
-  type JobResult = Either[ProcessingError, SimpleJob]
+  def receive = ???
 }
