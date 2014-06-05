b4b41697a09904f02355bf9cc169dab5cdd3d754
Dmitry Ivanov <id.ajantis@gmail.com>
2014-06-05 16:18:19 +0200

    Runner class implementation

--- service/src/main/scala/com/sample/service/ServiceMain.scala
+++ service/src/main/scala/com/sample/service/ServiceMain.scala
@@ -2,17 +2,37 @@ package com.sample.service
 
 import java.util.concurrent.TimeUnit
 
+import scala.util.control.Exception.catching
 import scala.concurrent.duration._
-import akka.actor.ActorSystem
-import com.typesafe.config.Config
+
+import akka.actor.{ Props, ActorSystem }
+import akka.io.IO
+import spray.can.Http
+import com.typesafe.config.{ ConfigException, Config, ConfigFactory }
+
+import http.ApiActor
 
 object ServiceMain extends App {
   implicit val system = ActorSystem("simple-service")
 
-  // TODO setup & startup
+  catching(classOf[ConfigException]).either(Settings(ConfigFactory.load)) match {
+
+    case Right(settings) =>
+      startup(system, settings)
+
+    case Left(error) =>
+      println(s"Invalid configuration. Details: ${error.getMessage}")
+      system.shutdown()
+      system.awaitTermination(2 seconds)
+      System.exit(1)
+  }
 
   private[this] def startup(implicit system: ActorSystem, settings: Settings): Unit = {
-    // TODO
+    val apiActor = system.actorOf(Props(new ApiActor(settings)))
+
+    IO(Http) ! Http.Bind(apiActor, interface = settings.Host, port = settings.Port)
+
+    println(s"Simple service is started. Listening on ${settings.Host}:${settings.Port}")
   }
 }
 
