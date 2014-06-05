0b61a666c27b6916e404a596265db3362e5a66d7
Dmitry Ivanov <id.ajantis@gmail.com>
2014-06-05 16:26:28 +0200

    finishing processing actor implementation

--- service/src/main/scala/com/sample/service/processor/ProcessingActor.scala
+++ service/src/main/scala/com/sample/service/processor/ProcessingActor.scala
@@ -1,17 +1,43 @@
-
 package com.sample.service
 package processor
 
-import akka.actor.{ ActorLogging, Actor }
+import scala.concurrent.duration._
+import akka.actor._
 import domain.SimpleJob
+import ProcessingActor._
 
-class ProcessingActor extends Actor with ActorLogging {
+class ProcessingActor(timeout: FiniteDuration, resultCallback: JobResult => Unit) extends Actor with ActorLogging {
+
+  lazy val worker = createWorker
+
+  context.setReceiveTimeout(timeout)
 
   override def preStart() {
     log.debug("Starting new processor...")
   }
 
-  def receive = ???
+  def receive = {
+    case job: SimpleJob    => worker ! job
+
+    case result: JobResult => finish(result)
+
+    case ReceiveTimeout =>
+      log.error("Got the timeout in processor. Aborting...")
+      finishWithError(ProcessingError("Processing failed with timeout."))
+  }
+
+  private[processor] def finish(result: JobResult): Unit = {
+    log.info("Job is done.")
+    resultCallback(result)
+    self ! PoisonPill
+  }
+
+  private[processor] def finishWithError(error: ProcessingError): Unit = {
+    resultCallback(Left(error))
+    self ! PoisonPill
+  }
+
+  private[processor] def createWorker: ActorRef = context.actorOf(Props(new Worker))
 }
 
 object ProcessingActor {
